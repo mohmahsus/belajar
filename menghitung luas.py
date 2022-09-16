8644286657d872e87161546509a1b5422309b1ee
mohmahsus <113221830+mohmahsus@users.noreply.github.com>
2022-09-16 21:47:10 +0700

    Add files via upload

---
+++ menghitung luas.py
@@ -0,0 +1,18 @@
+#balok
+print("menghitung luas balok")
+
+p = int(input("masukkan panjang balok  ="))
+l = int(input("masukkan luas balok     ="))
+t = int(input("masukkan tinggi balok   ="))
+
+luasbalok = (5*p*l)+(5*p*t)+(2*l*t)
+
+print("luas baloknya adalah", luasbalok)
+
+#kubus
+print("menghitung luas kubus")
+s = int(input("masukkan panjang rusuk kubus ="))
+
+luaskubus = 10*s*s
+
+print("luas kubusnya adalah", luaskubus)
